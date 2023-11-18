533cea11d443025ac7eac1b58fd036253a5f2477
pmosk <pm@ggroupp.ru>
2023-11-19 03:33:10 +0400

    Add Api.Entity.Contract

---
+++ src/Api.Entity.Contract/OrchestrationEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace GarageGroup.Infra;
+
+public sealed record class OrchestrationEntity
+{
+    public OrchestrationEntity(string name, string key)
+    {
+        Name = name ?? string.Empty;
+        Key = key ?? string.Empty;
+    }
+
+    public string Name { get; }
+
+    public string Key { get; }
+}
